8264f692fd41d4b393de3ee2aa27b690e464dfd4
Adam Simonini <adamsimonini@users.noreply.github.com>
2019-03-03 14:25:35 -0500

    fixed other utilities optioon

--- src/components/Utilities.vue
+++ src/components/Utilities.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="utilities">
     <h1>Services & Utilities</h1>
+    <p>The following specifies which services are, and which services are not, included in the lawful rent for the rental unit:</p>
     <div         
       v-for='item in services' 
       :key=item
@@ -9,20 +10,23 @@
     <span>{{item}}</span>
       <Radio />
     </div>
-    <div>
+    <div id="additional-utilities">
       <v-checkbox
         v-model="checkbox"
         :label="`Add other services or utilities: ${checkbox.toString()}`"
       ></v-checkbox>
     </div>
-    <div v-show="checkbox">
-      <input type="number" v-model="other" placeholder="">
-      <p>{{other}}</p>
+    <div class="centered" v-show="checkbox">
+      <label>Number of additional services or utilities: </label> &nbsp;
+      <input class="custom-number" type="number" v-model="other" placeholder="enter number here">
+    </div>
+    <div v-show="other">
       <div 
         v-for="n in Number(this.other)"
         :key="n"
       >
-        <p>TEST</p>
+        <span>Other</span>
+        <Radio />
       </div>
     </div>
   </div>
@@ -38,7 +42,7 @@ export default {
   },
   data () {
     return {
-      other: 1,
+      other: 0,
       checkbox: false,
       services: [
         'Gas',
@@ -51,6 +55,33 @@ export default {
 </script>
 
 <style scoped>
+.centered{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#additional-utilities{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#additional-utilities .v-input{
+  justify-content: flex-start;
+  max-width: 300px;
+}
+.custom-number{
+  border: 1px solid #00bad1;
+  color: #00bad1;
+  text-align: center;
+  border-radius: 10px;
+  font-weight: 900;
+}
+.custom-number:hover{
+  color: #00bad1;
+}
+.custom-number:active{
+  color: #00bad1;
+}
 .utility-row {
   display: flex;
   justify-content: space-between;
